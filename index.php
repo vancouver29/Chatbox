9b52e7eeec0cc45abad0a9121842eb919b2afd6d
vancouver29 <vancouver2990@gmail.com>
2020-04-14 22:04:07 +0200

    added new style

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
         <title>CHAT BOX</title>
-        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     <body>
         <div id="container">
@@ -12,20 +12,20 @@
             </header>
             <div id="chats">
                 <ul>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-                    <li class="chat"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+                    <li class="chat"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
                 </ul>
             </div>
             <div id="input">
                 <form method="post" action="process.php">
-                    <input type="text" name="user" placeholder="Enter Your Name" />
-                    <input type="text" name="message" placeholder="Enter A Message" />
-                    <br />
-                    <input type="submit" name="submit" value="Submit" />
+                    <input type="text" name="user" placeholder="Enter Your Name">
+                    <input type="text" name="message" placeholder="Enter A Message">
+                    <br>
+                    <input class="submit-btn" type="submit" name="submit" value="Submit">
                 </form>
             </div>
         </div>
